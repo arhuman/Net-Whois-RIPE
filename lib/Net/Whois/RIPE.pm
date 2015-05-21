af9d205efcaf626e5e6317ca70761507662ae7ee
Arnaud (Arhuman) ASSAD <arhuman@gmail.com>
2015-05-21 10:09:33 +0200

    Bump version to 2.005006

--- lib/Net/Whois/RIPE.pm
+++ lib/Net/Whois/RIPE.pm
@@ -28,11 +28,11 @@ Net::Whois::RIPE - a pure-Perl implementation of the RIPE Database client.
 
 =head1 VERSION
 
-Version 2.005005
+Version 2.005006
 
 =cut
 
-our $VERSION = 2.005005;
+our $VERSION = 2.005006;
 
 =head1 SYNOPSIS
 
